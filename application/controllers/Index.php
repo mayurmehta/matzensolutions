a5fa692aadca25cee6a0b43f28e64fa52bdcafdc
mayur champaneria <mayur.itpg@gmail.com>
2017-08-26 22:12:17 +0530

    set default controller and change home page title

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -7,14 +7,13 @@ class Index extends CI_Controller
 
 	public function __construct(){
 		parent::__construct();
-		// $this->load->model('user_model');
-		$this->data['page_title'] = "Welcome to Index";
+		
 		$url_method = $this->router->fetch_method(); // controller action {method}
-        $this->data['url'] = $url_method;
+		$this->data['url'] = $url_method;
 	}
 
 	public function index(){
-		$this->data['page_title'] ="Login to MMT v1.2 Admin Panel";
+		$this->data['page_title'] ="Home | Matzen Solution";
 		$this->template->load("home_template","home/index", $this->data);
 	}
 
